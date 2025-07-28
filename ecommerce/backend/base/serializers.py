5c1386bd4a6535661910ae0e2d20a2526c54a388
waliqs <>
2025-07-28 14:07:56 +0500

    Initial commit for Django + React e-commerce project

--- ecommerce/backend/base/serializers.py
+++ ecommerce/backend/base/serializers.py
@@ -1,4 +1,3 @@
-from asyncore import read
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Product, Order, OrderItem, ShippingAddress, Review
